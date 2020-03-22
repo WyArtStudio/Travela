1bb9d5e08b3bb02076f9c403257a0b7c08e93873
User <reiizzakeira123>
2020-03-23 01:22:50 +0700

    Initial commit

---
+++ app/src/main/java/com/wahyuhw/travela/DashboardActivity.java
@@ -0,0 +1,14 @@
+package com.wahyuhw.travela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
---
+++ app/src/main/java/com/wahyuhw/travela/MainActivity.java
@@ -0,0 +1,31 @@
+package com.wahyuhw.travela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn :
+                Intent move = new Intent(MainActivity.this, DashboardActivity.class);
+                startActivity(move);
+                break;
+        }
+    }
+}
